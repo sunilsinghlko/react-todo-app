16ee2f50f94cf13b39c581ecff5b05638a99cb3b
sunilsinghlko <sunilpdf94@gmail.com>
2024-03-05 17:53:38 +0530

    todo-react-app

--- src/store/todo-items-store.jsx
+++ src/store/todo-items-store.jsx
@@ -1,3 +1,66 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
-export const TodoItemsContext = createContext([]);
+export const TodoItemsContext = createContext({
+  todoItems: [],
+  addNewItem: () => {},
+  DeleteItem: () => {},
+});
+
+const todoItemsReducers = (currTodoItem, action) => {
+  let newTodoItems = currTodoItem;
+  if (action.type == "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItem,
+      { name: action.payload.itemName, date: action.payload.DueDate },
+    ];
+  } else if (action.type == "DELETE_ITEM") {
+    newTodoItems = currTodoItem.filter((item) => {
+      return item.name !== action.payload.itemName;
+    });
+  }
+  return newTodoItems;
+};
+const TodoItemsContextProvider = ({ children }) => {
+  // let [todoItems, settodoItem] = useState([]);
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducers, []);
+  const addNewItem = (itemName, DueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        DueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+    /* settodoItem((currValue) => {
+    const newTodoItem = [...currValue, { name: itemName, date: DueDate }];
+    return newTodoItem;
+  });*/
+  };
+  const DeleteItem = (todoItemName) => {
+    const newItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName: todoItemName,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+    /*const newTodoItems = todoItems.filter((item) => {
+    return item.name !== todoItemName;
+  });
+  settodoItem(newTodoItems);*/
+  };
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems: todoItems,
+        addNewItem: addNewItem,
+        DeleteItem: DeleteItem,
+      }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+export default TodoItemsContextProvider;
